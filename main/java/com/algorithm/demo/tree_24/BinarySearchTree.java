b97b28552b1a753f041a97704f55dd2bff2330ed
hiway <okbutwhy>
2020-07-18 14:07:52 +0800

    二叉树增加前序中序后序层级遍历

--- main/java/com/algorithm/demo/tree_24/BinarySearchTree.java
+++ main/java/com/algorithm/demo/tree_24/BinarySearchTree.java
@@ -1,18 +1,21 @@
 package com.algorithm.demo.tree_24;
 
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * 二叉树
  */
 public class BinarySearchTree {
-    Node head;
+    Node root;
 
     public void insert(int data){
-        if(head==null){
-            head = new Node(data);
+        if(root==null){
+            root = new Node(data);
             return;
         }
 
-        Node node = head;
+        Node node = root;
         while(node!=null){
             if(data>node.value){
                 if(node.right==null){
@@ -30,8 +33,9 @@ public class BinarySearchTree {
         }
     }
 
+
     public void delete(int data){
-        Node p = head;
+        Node p = root;
         Node pp = null;
         while(p!=null&&p.value!=data){
             pp = p;
@@ -62,9 +66,80 @@ public class BinarySearchTree {
         else if (p.right != null) child = p.right;
         else child = null;
 
-        if (pp == null) head = child; // 删除的是根节点
+        if (pp == null) root = child; // 删除的是根节点
         else if (pp.left == p) pp.left = child;
         else pp.right = child;
+    }
+
+
+    /**
+     * 根结点D、左子树L、右子树R
+     * D=Degree
+     * L=Left
+     * R=Right
+     */
+
+    /**
+     * 前序遍历
+     */
+    public void DLR(Node node){
+        if(node==null) return;
+        System.out.print(node.value+ ",");
+        DLR(node.left);
+        DLR(node.right);
+    }
+
+    /**
+     * 中序遍历
+     * @param node
+     */
+    public void LDR(Node node){
+        if(node==null) return;
+        LDR(node.left);
+        System.out.print(node.value+ ",");
+        LDR(node.right);
+    }
+
+    /**
+     * 后序遍历
+     * @param node
+     */
+    public void LRD(Node node){
+        if(node==null) return;
+        LRD(node.left);
+        LRD(node.right);
+        System.out.print(node.value+ ",");
+    }
+
+    /**
+     * 层序遍历
+     */
+    public void FloorPrint() {
+        Queue<Node> queue = new LinkedBlockingQueue();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            System.out.print(node.value+",");
+            if(node.left!=null) queue.add(node.left);
+            if(node.right!=null) queue.add(node.right);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(1);
+        tree.insert(4);
+        tree.insert(7);
+        tree.insert(9);
+        tree.insert(8);
+        tree.insert(6);
+        tree.insert(3);
+        tree.insert(5);
+        //tree.DLR(tree.root);
+        //tree.LDR(tree.root);
+        //tree.LRD(tree.root);
+        tree.FloorPrint();
     }
 
 
